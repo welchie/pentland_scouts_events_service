0d7a75f274722c135287ec5d3a3d3cf8ff213bd0
Chris Welch <chris.welch@currenthealth.com>
2024-02-07 19:20:07 +0000

    Updating the build version

--- docker_create-dev.sh
+++ docker_create-dev.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile-dev -t welchie99/scoutsevents:0.0.16-SNAPSHOT-DEV .
+docker build -f Dockerfile-dev -t welchie99/scoutsevents:0.0.17-SNAPSHOT-DEV .
--- docker_create-prod.sh
+++ docker_create-prod.sh
@@ -1,1 +1,1 @@
-docker build -f Dockerfile-prod -t welchie99/scoutsevents:0.0.16-SNAPSHOT-PROD .
+docker build -f Dockerfile-prod -t welchie99/scoutsevents:0.0.17-SNAPSHOT-PROD .
